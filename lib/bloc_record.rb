b472933097ce6f3d77808f9bb8841b25d0827d17
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-07-26 17:32:37 -0400

    Initial commit cp2

---
+++ lib/bloc_record.rb
@@ -0,0 +1,11 @@
+require 'bloc_record/base'
+
+module BlocRecord
+    def self.connect_to(filename)
+        @database_filename = filename
+    end
+
+    def self.database_filename
+        @database_filename
+    end
+end
